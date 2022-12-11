d1f4bbbb8522eae0e53f81be0daf34732327dd5e
SLG\ciordanidis <ciordanidis@singularlogic.eu>
2022-12-11 22:58:37 +0200

    express ngrx install

--- apps/nx-northwind-app/src/app/app.component.ts
+++ apps/nx-northwind-app/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nx-northwind-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'nx-northwind-app';
